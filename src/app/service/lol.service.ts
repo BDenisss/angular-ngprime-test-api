82d2207ada69da949f325886f2b1e6047c0ff74c
BDenisss <bucspun.d@gmail.com>
2024-02-08 16:42:02 +0100

    :sparkles: add services (api)

---
+++ src/app/service/lol.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LolService {
+  private apiKey = "RGAPI-35e08d4c-694d-4358-b4e5-fe6f4bc91c2a";
+
+  constructor(private http: HttpClient) { 
+  }
+
+  getChampionRotation(){
+    const url = '/api/val/content/v1/contents?locale=fr-FR?api_key=' + this.apiKey;
+    const url2 = 'https://fakestoreapi.com/products'
+    return this.http.get(url2);
+  }
+  
+  
+}
